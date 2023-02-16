9c5a8dd547dbf23dc3bc637a9db195ce4dfbab3b
noisyboy25 <dj402and@gmail.com>
2023-02-16 22:26:05 +0300

    Add gorm

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
-    "log"
+	"log"
+	"os"
 
-    "github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+var dsn = os.Getenv("DB_URL")
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-    app := fiber.New()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	if err != nil {
+		panic("failed to connect database")
+	}
 
-    log.Fatal(app.Listen(":3000"))
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		var products = []Product{}
+		result := db.Find(&products)
+		if result.Error != nil {
+			log.Println(result.Error)
+		}
+		return c.JSON(products)
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
